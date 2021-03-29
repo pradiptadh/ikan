98960feca7884e3bbe5cd15082d451f5a4005f5a
pradiptadh <haryadipradipta@gmail.com>
2021-03-29 20:29:06 +0700

    commit

---
+++ test2.php
@@ -0,0 +1,19 @@
+<html>
+<head>
+</head>
+<body bgcolor="red">
+
+<center>
+<?php
+for ($i=1; $i<=5 ; $i++) { 
+    print("ini adalah baris ke $i <br>");
+}
+
+?>
+</center>
+
+
+
+</body>
+
+</html>
